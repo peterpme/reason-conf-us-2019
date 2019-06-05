7e302a85a536ac582a26335618a27576a128d5b9
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-06-05 18:55:54 -0500

    added speaker grid'

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,7 +6,31 @@ import { make as Logo } from '../components/Logo.bs'
 import { make as NavBar } from '../components/NavBar.bs'
 import { make as SpeakerCard } from '../components/SpeakerCard.bs'
 import { make as HeroGraphic } from '../components/HeroGraphic.bs'
+import { make as SpeakerGrid } from '../components/SpeakerGrid.bs'
 
+const speakers = [
+    {
+        speakerName:"Fuck" + "\n" +"Off",
+        title:"Software Engineer",
+        company:"Facebook",
+        bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu imperdiet risus. Nullam ac elit id metus tempor porttitor. Curabitur aliquam, lacus non varius ultrices, tortor diam mollis quam, ornare ultricies tortor leo id libero.",
+        imageUrl:"https://images.unsplash.com/photo-1559697242-07e90b48b9fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        speakerName:"Fuck" + "\n" +"Off",
+        title:"Software Engineer",
+        company:"Facebook",
+        bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu imperdiet risus. Nullam ac elit id metus tempor porttitor. Curabitur aliquam, lacus non varius ultrices, tortor diam mollis quam, ornare ultricies tortor leo id libero.",
+        imageUrl:"https://images.unsplash.com/photo-1559697242-07e90b48b9fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        speakerName:"Fuck" + "\n" +"Off",
+        title:"Software Engineer",
+        company:"Facebook",
+        bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu imperdiet risus. Nullam ac elit id metus tempor porttitor. Curabitur aliquam, lacus non varius ultrices, tortor diam mollis quam, ornare ultricies tortor leo id libero.",
+        imageUrl:"https://images.unsplash.com/photo-1559697242-07e90b48b9fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    }
+]
 export const IndexPageTemplate = () => (
   <div>
     <Helmet>
@@ -16,6 +40,7 @@ export const IndexPageTemplate = () => (
     <NavBar className="temp"/>
     <HeroGraphic />
     <SpeakerCard imageUrl="https://images.unsplash.com/photo-1559489744-3492970a72f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80" speakerName={"Fuck" + "\n" +"Off"} title="Software Engineer" company="Draftbit" bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu imperdiet risus. Nullam ac elit id metus tempor porttitor. Curabitur aliquam, lacus non varius ultrices, tortor diam mollis quam, ornare ultricies tortor leo id libero."/>
+    <SpeakerGrid speakers={speakers} />
   </div>
 )
 
