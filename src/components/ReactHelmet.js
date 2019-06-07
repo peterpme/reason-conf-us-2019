a9f7109932ff386bfe185f1a735c9514071b3888
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-06-07 18:54:31 -0500

    Centered shit... it's pretty dirty tbh

--- src/components/ReactHelmet.js
+++ src/components/ReactHelmet.js
@@ -5,6 +5,7 @@ const ReactHelmet= () => {
     return (
         <Helmet>
             <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
+            <style>{'body { display: flex; flex-direction: column; align-items:center;  }'}</style>
         </Helmet>
     )
 }
