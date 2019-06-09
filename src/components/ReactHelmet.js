ebe33fbdb93f9cac580a6aa4c0a70398f459d3d1
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-06-09 14:42:04 -0500

    Added the background gradient

--- src/components/ReactHelmet.js
+++ src/components/ReactHelmet.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import {Helmet} from 'react-helmet'
+import React from "react"
+import { Helmet } from "react-helmet"
 
-const ReactHelmet= () => {
-    return (
-        <Helmet>
-            <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
-            <style>{'body { display: flex; flex-direction: column; align-items:center; background-color: #000000; }'}</style>
-        </Helmet>
-    )
+const ReactHelmet = () => {
+  return (
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Montserrat&display=swap"
+        rel="stylesheet"
+      />
+      <style>
+        {
+          "body { display: flex; flex-direction: column; align-items:center; background: linear-gradient(159.38deg, #3A7EDD -2.87%, #0A1423 65.41%);}"
+        }
+      </style>
+    </Helmet>
+  )
 }
 
 export default ReactHelmet
