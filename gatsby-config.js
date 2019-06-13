7f899a3c02e4cdb4af960a7477f7c07dffeef9d6
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-06-12 19:37:34 -0500

    fix meta manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,24 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#000",
+        theme_color: "#DD4B39",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/img/logo.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`
+      }
+    },
+    {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import useSiteMetadata from "./SiteMetadata"
 import "./all.sass"
 import "./Layout.scss"
 
-const TemplateWrapper = ({ children }) => {
+export default function TemplateWrapper({ children }) {
   const { title, description } = useSiteMetadata()
   return (
     <React.Fragment>
@@ -14,14 +14,6 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-
-        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-
-        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-        <meta name="theme-color" content="#fff" />
-
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
@@ -33,5 +25,3 @@ const TemplateWrapper = ({ children }) => {
     </React.Fragment>
   )
 }
-
-export default TemplateWrapper
