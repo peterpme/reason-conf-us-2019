05c1a301d36cd59e33418fa20c02888b19e697e1
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-06-19 15:29:47 -0500

    cleanup

--- src/pages/speakers/index.js
+++ src/pages/speakers/index.js
@@ -1,36 +1,10 @@
 import React from "react"
-
 import Layout from "../../components/Layout"
-import BlogRoll from "../../components/BlogRoll"
 
-export default class SpeakerIndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`
-          }}>
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-              backgroundColor: "#f40",
-              color: "white",
-              padding: "1rem"
-            }}>
-            Speakers
-          </h1>
-        </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <BlogRoll />
-            </div>
-          </div>
-        </section>
-      </Layout>
-    )
-  }
+export default function SpeakersPage() {
+  return (
+    <Layout>
+      <h1>Speakers will be announced soon</h1>
+    </Layout>
+  )
 }
