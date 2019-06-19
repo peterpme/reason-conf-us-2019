f3d5088c621ef302980d374b739077dbf9a94f13
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-06-19 16:01:04 -0500

    stub out sponsors / workshops

---
+++ src/pages/sponsors/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../../components/Layout"
+
+export default function SponsorsPage() {
+  return (
+    <Layout>
+      <h1>Sponsors will be announced soon</h1>
+    </Layout>
+  )
+}
---
+++ src/pages/workshops/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../../components/Layout"
+
+export default function WorkshopsPage() {
+  return (
+    <Layout>
+      <h1>Workshops will be announced soon</h1>
+    </Layout>
+  )
+}
