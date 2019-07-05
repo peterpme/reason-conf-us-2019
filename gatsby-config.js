a7878444e27d36d79be87031308c17567db38bf3
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-07-04 22:49:02 -0500

    remove netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,12 +142,6 @@ module.exports = {
       }
     },
     {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
-      }
-    },
-    {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
